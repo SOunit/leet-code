3dadb19429c53f585944ec578d77a26f19c977cc
SOunit <workbookwork7@gmail.com>
2022-08-11 13:35:11 -0700

    add sample

---
+++ 26-removeDuplicates.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  nums = [];
+
+  return nums.length;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
---
+++ initials/26-removeDuplicates.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {};
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
