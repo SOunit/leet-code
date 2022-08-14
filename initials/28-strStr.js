765d2e4d5c9fffb0d60b233afb1bd4e77f6846ac
SOunit <workbookwork7@gmail.com>
2022-08-14 13:03:52 -0700

    add sample

---
+++ initials/28-strStr.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {};
+
+console.log(strStr("hello", "ll"));
+console.log(strStr("aaaaa", "bba"));
