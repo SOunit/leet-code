1577b7ad391b3098a63b1133302ad234285d3f7a
SOunit <workbookwork7@gmail.com>
2022-08-15 14:14:00 -0700

    add sample

--- temp.java
+++ temp.java
@@ -1,24 +1,17 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-      int n = digits.length;
-  
-      // move along the input array starting from the end
-      for (int idx = n - 1; idx >= 0; --idx) {
-        // set all the nines at the end of array to zeros
-        if (digits[idx] == 9) {
-          digits[idx] = 0;
-        }
-        // here we have the rightmost not-nine
-        else {
-          // increase this rightmost not-nine by 1
-          digits[idx]++;
-          // and the job is done
-          return digits;
-        }
-      }
-      // we're here because all the digits are nines
-      digits = new int[n + 1];
-      digits[0] = 1;
-      return digits;
+  public int mySqrt(int x) {
+    if (x < 2) return x;
+
+    long num;
+    int pivot, left = 2, right = x / 2;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      num = (long)pivot * pivot;
+      if (num > x) right = pivot - 1;
+      else if (num < x) left = pivot + 1;
+      else return pivot;
     }
+
+    return right;
   }
+}
